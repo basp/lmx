8820af33fa3dbe15f2b875ac5eca3dd8d3a63cd6
Bas Pennings <baspster@gmail.com>
2019-02-03 15:09:12 +0100

    It kinda works

--- src/lmx.nim
+++ src/lmx.nim
@@ -23,47 +23,59 @@ when is_main_module:
       b = int(255.99 * c.b).clamp(0, 255)
     (r, g, b)
 
-  var checkers = checkers_pattern(color(1, 1, 1), color(0, 0, 0.0))
-  checkers.transform = scaling(2.0, 2.0, 2.0)
+  let checkers: Pattern = checkers_pattern(color(1, 1, 1), color(0, 0, 0.0))
+  checkers.transform = identity
 
-  let floor: Shape = plane()
-  floor.material = material()
-  floor.material.pattern = some(Pattern(checkers))
+  let 
+    p1: Shape = plane()
+    p2: Shape = plane()
+    p3: Shape = plane()
+    p4: Shape = plane()
+    p5: Shape = plane()
+    p6: Shape = plane()
 
-  let s1: Shape = sphere()
-  s1.transform = translation(0, 1, 0)
-  s1.material = material()
-  s1.material.color = color(0.02, 0, 0)
-  s1.material.specular = 0.9
-  s1.material.shininess = 350
-  s1.material.ambient = 0.06
-  s1.material.diffuse = 0.07
-  s1.material.reflective = 0.13
-  s1.material.transparency = 0.98
-  #s1.material.refractive_index = 1.00029 # air
-  s1.material.refractive_index = 1.52 # water
+  p1.material = material()
+  p1.material.pattern = some(checkers)
+  p1.transform = identity
 
-  let s2: Shape = sphere()
-  s2.transform = translation(-2, 0.5, 2.5) * scaling(0.5, 0.5, 0.5)
-  s2.material = material()
-  s2.material.color = color(0.3, 0.1, 0.8)
-  s2.material.specular = 0.6
+  p2.material = material()
+  p2.material.pattern = some(checkers)
+  p2.transform = translation(-7, 0, 0) * rotation_z(-PI / 2)
 
-  let s3: Shape = sphere()
-  s3.transform = translation(2, 0.5, -2.75) * scaling(0.5, 0.5, 0.5)
-  s3.material = material()
-  s3.material.color = color(0.3, 0.8, 0.2)
-  s3.material.specular = 0.6
+  p3.material = material()
+  p3.material.pattern = some(checkers)
+  p3.transform = translation(0, 0, 7) * rotation_x(PI / 2)
 
-  let light = point_light(point(-10, 10, -10), color(1, 1, 1))
+  p4.material = material()
+  p4.material.pattern = some(checkers)
+  p4.transform = translation(7, 0, 0) * rotation_z(PI / 2)
+
+  p5.material = material()
+  p5.material.pattern = some(checkers)
+  p5.transform = translation(0, 0, -7) * rotation_x(-PI / 2)
+
+  p6.material = material()
+  p6.material.pattern = some(checkers)
+  p6.transform = translation(0, 7, 0) * rotation_x(PI)
+
+  let s: Shape = sphere()
+  s.material = material()
+  s.material.color = color(0.0, 0.0, 0.0)
+  s.material.reflective = 0.4
+  s.material.specular = 0.4
+  s.material.diffuse = 0.001
+  s.material.ambient = 0.001
+  s.transform = translation(0, 1.0, 0)  
+
+  let light = point_light(point(1, 3, -3), color(1, 1, 1))
 
   var w = world()
   w.lights = @[light]
-  w.objects = @[floor, s1, s2]
+  w.objects = @[s, p1, p2, p3, p4, p5, p6]
 
   let c = camera(400, 200, PI / 3)
-  c.transform = view_transform(point(4, 5.5, -3.5),
-                               point(0, 0.5, 0),
+  c.transform = view_transform(point(2.5, 3.0, -2.5),
+                               point(0, 1.0, 0),
                                vector(0, 1, 0))
   
   let start = now()
